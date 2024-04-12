390389afc73e9e77ecd7d2b13d88f3fc6f54ca0c
Arjun <arjun8107@gmail.com>
2024-04-13 00:02:26 +0530

    added big and small tags

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use ::std::env;
 mod lexer;
 mod networking;
 
+use itertools::{self, Itertools};
 use lexer::{Tag, Text, Token};
 use macroquad::prelude::*;
-
 const SCROLL_DISTANCE: f32 = 100.0;
 
 struct DefaultFont {
@@ -42,58 +42,81 @@ impl<'a> Default for DefaultFont {
     }
 }
 
-fn layout<'a>(
-    tokens: &Vec<Token>,
-    font: &'a DefaultFont,
-) -> Vec<(f32, f32, String, TextDimensions, &'a Font)> {
-    let mut display_list: Vec<(f32, f32, String, TextDimensions, &'a Font)> = Vec::new();
-    let mut x = 0.0;
-    let mut y = 10.0;
-    let mut style = "Roman";
-    let mut weight = "Normal";
-    for token in tokens {
-        let c = match &token {
-            Token::Text(Text { text }) => text,
-            Token::Tag(Tag { tag }) => {
-                if tag == "i" || tag == "em" {
-                    style = "Italic";
-                } else if tag == "/i" || tag == "/em" {
-                    style = "Roman";
-                } else if tag == "b" || tag == "strong" {
-                    weight = "Bold";
-                } else if tag == "/b" || tag == "/strong" {
-                    weight = "Normal";
-                } else {
+struct Layout<'a> {
+    display_list: Vec<(f32, f32, u16, String, TextDimensions, &'a Font)>,
+    x: f32,
+    y: f32,
+    style: &'a str,
+    weight: &'a str,
+}
+
+impl<'a> Layout<'a> {
+    fn new(tokens: &Vec<Token>, font: &'a DefaultFont) -> Self {
+        let mut display_list: Vec<(f32, f32, u16, String, TextDimensions, &'a Font)> = Vec::new();
+        let mut x = 0.0;
+        let mut y = 10.0;
+        let mut font_size: u16 = 16;
+        let mut style = "roman";
+        let mut weight = "normal";
+        for token in tokens {
+            let c = match &token {
+                Token::Text(Text { text }) => text,
+                Token::Tag(Tag { tag }) => {
+                    if tag == "i" || tag == "em" {
+                        style = "italic";
+                    } else if tag == "/i" || tag == "/em" {
+                        style = "roman";
+                    } else if tag == "b" || tag == "strong" {
+                        weight = "bold";
+                    } else if tag == "/b" || tag == "/strong" {
+                        weight = "normal";
+                    } else if tag == "small" {
+                        font_size -= 2;
+                    } else if tag == "/small" {
+                        font_size += 2;
+                    } else if tag == "big" {
+                        font_size += 4;
+                    } else if tag == "/big" {
+                        font_size -= 4;
+                    } else {
+                    }
+                    ""
                 }
-                ""
+            };
+            let cfont = if style == "italic" && weight == "bold" {
+                &font.bold_italic
+            } else if weight == "bold" {
+                &font.bold
+            } else if style == "italic" {
+                &font.italic
+            } else {
+                &font.roman
+            };
+            let space_measure = measure_text(" ", Some(cfont), font_size, 1.0);
+            let empty_measure = measure_text("", Some(cfont), font_size, 1.0);
+            let c = html_escape::decode_html_entities(c);
+
+            for word in c.split_whitespace() {
+                let measure: TextDimensions = measure_text(word, Some(cfont), font_size, 1.0);
+                if x + measure.width >= screen_width() {
+                    y += 18.0 * 1.25;
+                    x = 0.0;
+                }
+                display_list.push((x, y, font_size, word.to_string(), measure, cfont));
+                x += measure.width + space_measure.width;
             }
-        };
-        let cfont = if style == "Italic" && weight == "Bold" {
-            &font.bold_italic
-        } else if weight == "Bold" {
-            &font.bold
-        } else if style == "Italic" {
-            &font.italic
-        } else {
-            &font.roman
-        };
-        let space_measure = measure_text(" ", Some(cfont), 16, 1.0);
-        let empty_measure = measure_text("", Some(cfont), 16, 1.0);
-        let c = html_escape::decode_html_entities(c);
-        for word in c.split_whitespace() {
-            let measure: TextDimensions = measure_text(word, Some(cfont), 16, 1.0);
-            if x + measure.width >= screen_width() {
-                y += 18.0 * 1.25;
-                x = 0.0;
+            if c.split_whitespace().count() <= 0 {
+                display_list.push((x, y, font_size, "".to_string(), empty_measure, cfont));
             }
-            display_list.push((x, y, word.to_string(), measure, cfont));
-            x += measure.width + space_measure.width;
         }
-        if c.split_whitespace().count() <= 0 {
-            display_list.push((x, y, "".to_string(), empty_measure, cfont));
+        Self {
+            display_list,
+            x,
+            y,
+            style,
+            weight,
         }
     }
-    display_list
 }
 
 #[macroquad::main("Text")]
@@ -115,7 +138,8 @@ async fn main() {
         } else if mouse_wheel_y > 0.0 {
             scroll -= SCROLL_DISTANCE;
         }
-        for (x, y, c, d, style) in layout(&text, &font) {
+        let layout = Layout::new(&text, &font);
+        for (x, y, font_size, c, d, style) in layout.display_list {
             if (y > scroll + screen_height()) || (y + d.height < scroll) {
                 continue;
             }
@@ -126,7 +150,7 @@ async fn main() {
                 y - scroll,
                 TextParams {
                     font: Some(&style),
-                    font_size: 16,
+                    font_size,
                     color: BLACK,
                     ..Default::default()
                 },
